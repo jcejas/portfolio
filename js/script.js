5016d3b0b5c0164c33d116355e717e8fa47a578f
jcejas <ciglesias@bigtree.com.ar>
2021-03-24 12:23:07 -0300

    Agregado de Boton On Off al Efecto de Sonido

--- js/script.js
+++ js/script.js
@@ -18,6 +18,17 @@ $("#ironman-button").on("click",function(){
 	$("#sound-effect").get(0).play();
 });
 
+$("#sound-control").on("click",function(){
+	var checked = $('#sound-button').prop('checked');
+	if (checked){
+		$("#sound-effect").prop("muted",true);
+		$(this).prop("title","Activar Sonido");
+	} else{
+		$("#sound-effect").prop("muted",false);
+		$(this).prop("title","Desactivar Sonido");
+	}
+});
+
 $(window).scroll(function(){
 	if ($("#menu").offset().top > 60) {
 		$("#menu").addClass("change-background");
